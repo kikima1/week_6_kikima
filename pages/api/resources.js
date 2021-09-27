64c792dfe23c9bdc43094d9beeaa22c591e84443
Miriam Block <kikima@mac.com>
2021-09-27 19:13:15 +0000

    first commit

---
+++ pages/api/resources.js
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import firebase from "../../lib/firebase";
+export default async function handler(req, res) {
+  try{
+    const snapshot = await firebase.collection("people").get();
+    let output = [];
+    snapshot.forEach(
+      (doc)=>{
+        output.push(
+          {
+            id: doc.id,
+            data: doc.data()
+          }
+        );
+      }
+    );
+    console.log(output);
+    res.statusCode = 200;
+    res.setHeader("Content-Type","application/json");
+    res.json({output});
+  }catch(err){
+    console.error(err);
+    res.status(500).end(err.message);
+  }
+}
